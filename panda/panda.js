cff320d24145f5ebca24ac43aae6b135d557cae7
LizFoo <liuz6@tcd.ie>
2022-03-01 20:40:25 +0000

    panda

---
+++ panda/panda.js
@@ -0,0 +1,18 @@
+$("#password").focusin(function() {
+    $('.login-box').addClass('up');
+
+}).focusout(function() {
+    $('.login-box').removeClass('up');
+
+})
+
+$(document).on('mouseover', function(e) {
+    let dw = $(document).width() / 10;
+    let dh = $(document).height() / 18;
+    let x = e.pageX / dw;
+    let y = e.pageY / dh;
+    $('.eye-ball').css({
+        left: x,
+        top: y
+    })
+})
